36bf43a67eb3ac346c8fa0ac85839f55c7cd1210
YunxiTang <54171095+YunxiTang@users.noreply.github.com>
2019-12-20 15:00:30 +0800

    Delete Flight_Controller.m

--- Flight_Controller.m
+++
@@ -1,9 +0,0 @@
-function [u] = Flight_Controller(t,model,x)
-%FLIGHT_CONTROLLER Change the landing angle
-%   Input: t --> time 
-%          x --> current state
-%   output: u --> landing angle = [THETA]
-dxc = x(3);
-
-end
-
